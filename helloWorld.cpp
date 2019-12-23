bc880721148b3372174e9bd61867c13abe452bf2
kilarinikhil <nikhilkilari.m2@gmail.com>
2019-12-23 21:16:12 +0530

    replaced

--- helloWorld.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>//preprocessor
-
-int main(){
-	std::cout<<"Hello World!\n";// print hello world!
-	// ">>" denotes insertion operator
-	
-	return 0;// useless for present case
-}
